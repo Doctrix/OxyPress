5fd91b44e21916ce6c3ba825302b414524f63a3d
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-05-03 07:40:22 +0200

    correction de la fonction rafraichir la barre du menu seulement 'changement de couleur'

--- wp-content/themes/oxy/options/apparence.php
+++ wp-content/themes/oxy/options/apparence.php
@@ -6,7 +6,11 @@ add_action('customize_register', function (WP_Customize_Manager $manager) {
 	]);
 	$manager->add_setting('header_background', [
 		'default' => '#000000',
+		'transport' => 'postMessage'
 		'sanitize_callback' => 'sanitize_hex_color'
 	]);
-	$manager->add_control(new WP_Customize_Color_Control();
+	$manager->add_control(new WP_Customize_Color_Control($manager, 'header_background', [
+		'section' => 'oxy_apparence',
+		'label' => 'Couleur de l\'en tête'
+	]));
 });
