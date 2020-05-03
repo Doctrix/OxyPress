e4cc20b198891b6025818477d7a14968ddb2c862
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-05-03 08:25:12 +0200

    maj de la fonction apparence

---
+++ wp-content/themes/oxy/assets/apparence.js
@@ -0,0 +1,9 @@
+﻿(function ($) {
+
+    wp.customize('header_background', function (value) {
+        value.bind(function (newVal) {
+            $('.navbar').attr('style', 'background:' + newVal + '!important')
+        });
+    });
+
+})(jQuery);
