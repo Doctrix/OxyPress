26104791b4c6e1228ab4015026f5c3040af8d0d0
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-05-03 08:35:25 +0200

    correction du fichier function

--- wp-content/themes/oxy/options/apparence.php
+++ wp-content/themes/oxy/options/apparence.php
@@ -1,9 +1,11 @@
 ﻿<?php
+// https://codex.wordpress.org/Plugin_API/Action_Reference/customize_register
 add_action('customize_register', function (WP_Customize_Manager $manager) {
 
 	$manager->add_section('oxy_apparence', [
-		'title' => 'Personnalisation de l\'apparence'
+		'title' => 'Personnalisation de l\'apparence',
 	]);
+
 	$manager->add_setting('header_background', [
 		'default' => '#000000',
 		'transport' => 'postMessage',
@@ -13,6 +15,7 @@ add_action('customize_register', function (WP_Customize_Manager $manager) {
 		'section' => 'oxy_apparence',
 		'label' => 'Couleur de l\'en tête'
 	]));
+
 });
 
 add_action('customize_preview_init', function () {
