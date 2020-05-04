08c102e353de283f8a130fd84021fa4844792c58
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-05-04 06:27:00 +0200

    securité api json disponible dans le plugin

--- wp-content/plugins/oxy-plugin/oxy-plugin.php
+++ wp-content/plugins/oxy-plugin/oxy-plugin.php
@@ -2,6 +2,7 @@
 /**
 * Plugin Name: Oxy Plugin
 */
+
 defined('ABSPATH') or die('oups il y a rien à voir');
 
 register_activation_hook(__FILE__, function() {
@@ -120,4 +121,26 @@ register_taxonomy('genre', 'post', [
 		'show_in_rest' => true,
 		'has_archive' => true,
 	]);
+});
+
+add_filter( 'rest_authentication_errors', function( $result ) {
+    // If a previous authentication check was applied,
+    // pass that result along without modification.
+    if ( true === $result || is_wp_error( $result ) ) {
+        return $result;
+    }
+
+    // No authentication has been performed yet.
+    // Return an error if user is not logged in.
+    if ( ! is_user_logged_in() ) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            __( 'You are not currently logged in.' ),
+            array( 'status' => 401 )
+        );
+    }
+
+    // Our custom authentication check should have no effect
+    // on logged-in requests
+    return $result;
 });
