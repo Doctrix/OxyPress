9487dd272a8986c67c190a2d357befb3faacebf4
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-06-30 22:17:22 +0200

    maj functions.php

--- wp-content/themes/oxy/functions.php
+++ wp-content/themes/oxy/functions.php
@@ -50,12 +50,12 @@ function oxy_supports () {
 }
 
 function oxy_register_assets () {
-    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', []);
-	wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
-	wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', [], false, true);
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css', []);
+	wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+	wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
 	if (!is_customize_preview()) {
 		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', [], false, true);
+		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
 	}
 	wp_enqueue_style('bootstrap',);
 	wp_enqueue_script('bootstrap');
