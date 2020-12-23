b4ab658fc07537096973711e608103ebd942a4cb
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-12-23 20:06:45 +0100

    update gutenberg

--- wp-content/themes/oxy/options/panel.php
+++ wp-content/themes/oxy/options/panel.php
@@ -6,5 +6,31 @@ function panel(){
 }
 
 function render_panel(){
-    
+    ?>
+    <div class="wrap theme-options-page">
+        <div id="icon-options-general" class="icon32"><br></div>
+        <h2>Réseaux</h2>
+        <form action="" method="POST">
+            <div class="theme-options-group">
+                <table cellspacing="0" class="widefat options-table">
+                    <thead>
+                        <tr>
+                            <th colspan="2">Mes réseaux</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <label for="twitter">Twitter</label>
+                            </td>
+                            <td>
+                                <input type="text" id="twitter" name="twitter" value="">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </form>
+    </div>
+    <?php
 }
