84b29dc156a77b8df79750178bd4286dc03fd0a9
Doctrix <34386780+Doctrix@users.noreply.github.com>
2020-12-24 01:55:57 +0100

    update footer

--- wp-content/themes/oxy/footer.php
+++ wp-content/themes/oxy/footer.php
@@ -1,22 +1,32 @@
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php
-			wp_nav_menu([
-				'theme_location' => 'footer',
-				'container' => false,
-				'menu_class' => 'navbar-nav mr-auto cadre-bloc-shadow bloc-large'
-				]);
-			the_widget(TwitchWidget::class, ['twitch' => '2HE5vRIx_RVGqQ'], ['before_widget' => '<div class="cadre-bloc-shadow">', 'after_widget' => '</div>']);
-			?>
-		<div class="cadre-bloc-shadow">
-			<h5><?= get_option('extra_infos') ?></h5>
-			<h6><a href="<?php echo get_option('extra_liens') ?>">En savoir plus</a></h6>
-			<h4>Projets en cours : <?= get_option('extra_events_titre') ?><br/>Sortie pr&eacute;vu : <?= get_option('extra_events_date') ?></h4>
-			<script type="text/javascript" src="<?= "//".$_SERVER['HTTP_HOST']."/compteur/"; ?>gcount.php?page=index"></script>
-
-		</div>
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<?php
+				wp_nav_menu([
+					'theme_location' => 'footer',
+					'container' => false,
+					'menu_class' => 'navbar-nav mr-auto cadre-bloc-shadow bloc-large'
+					]);
+				the_widget(TwitchWidget::class, ['twitch' => '2HE5vRIx_RVGqQ'], ['before_widget' => '<div class="cadre-bloc-shadow">', 'after_widget' => '</div>']);
+				?>
+			<div class="cadre-bloc-shadow">
+				<h3 style="text-align: center;"><?= get_option('extra_infos'); ?></h3>
+				<h4 style="text-align: center;"><a href="<?php echo get_option('extra_liens'); ?>">En savoir plus</a></h4>
+				<h5 style="text-align: center;">Projets en cours : <?= get_option('extra_events_titre'); ?>
+				<br/>Sortie pr&eacute;vu : <?= get_option('extra_events_date'); ?></h5>
+				<div style="text-align: right;"><script type="text/javascript" src="<?= "//".$_SERVER['HTTP_HOST']."/compteur/"; ?>gcount.php?page=index"></script></div>
+			</div>
+			<div class="cadre-bloc-shadow" style="text-align: center;">
+				<ul><strong><a href="http://oxygames.fr/<?= get_option('oxygames', ''); ?>" target="_blank">OxyGameS</a>
+					- <a href="http://server.oxygames.fr/<?= get_option('ogs', ''); ?>" target="_blank">OGS</a>
+					 - <a href="http://twitch.tv/<?= get_option('twitch', ''); ?>" target="_blank">Twitch</a>
+					  -	<a href="http://twitter.com/<?= get_option('twitter', ''); ?>" target="_blank">Twitter</a>
+					   - <a href="http://twitter.com/<?= get_option('facebook', ''); ?>" target="_blank">Facebook</a>
+					</strong><br><?= get_option('copyright', ''); ?>
+				</ul>
+			</div>
 		</footer>
-	</div><!-- #content -->	
+	</div>
+	<!-- #content -->
+	<br>
 <?php wp_footer() ?>
-
 </body>
 </html>
--- wp-content/themes/oxy/options/panel.php
+++ wp-content/themes/oxy/options/panel.php
@@ -2,10 +2,27 @@
 add_action('admin_menu','panel');
 
 function panel(){
-    add_menu_page('Réseau','Réseau','activate_plugins','panel','render_panel',null,84);
+    add_menu_page('Réseau', 'Réseau', 'activate_plugins', 'panel', 'render_panel', null, 84);
 }
 
 function render_panel(){
+    if(isset($_POST['panel_update'])){
+        if(!wp_verify_nonce($_POST['panel_noncename'], 'panel-key')){
+            die('Token non valide');
+        }
+        foreach($_POST['options'] as $name => $value){
+            if(empty($value)){
+                delete_option($name);
+            }else{
+                update_option($name, $value);
+            }
+        }
+        ?>
+        <div id="message" class="update fade">
+        <p>Options sauvegardées avec succes!</p>
+    </div>
+    <?php
+    }
     ?>
     <div class="wrap theme-options-page">
         <div id="icon-options-general" class="icon32"><br></div>
@@ -19,17 +36,45 @@ function render_panel(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                    <tr>
+                        <th scope="row">
+                                <label for="copyright">Copyright</label>
+                            </th>
                             <td>
-                                <label for="twitter">Twitter</label>
+                                <input type="text" id="copyright" name="options[copyright]" value="<?= get_option('copyright',''); ?>" size="75">
                             </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <label for="oxygames">OxyGameS</label>
+                            </th>
                             <td>
-                                <input type="text" id="twitter" name="twitter" value="">
+                                <input type="text" id="oxygames" name="options[oxygames]" value="<?= get_option('oxygames',''); ?>" size="75">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <label for="twitter">Twitter</label>
+                            </th>
+                            <td>
+                                <input type="text" id="twitter" name="options[twitter]" value="<?= get_option('twitter',''); ?>" size="75">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <label for="facebook">Facebook</label>
+                            </th>
+                            <td>
+                                <input type="text" id="facebook" name="options[facebook]" value="<?= get_option('facebook',''); ?>" size="75">
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            <input type="hidden" name="panel_noncename" value="<?= wp_create_nonce('panel-key'); ?>">
+            <p class="submit">
+                <input type="submit" name="panel_update" class="button-primary autowidth" value="Sauvegarder">
+            </p>
         </form>
     </div>
     <?php
